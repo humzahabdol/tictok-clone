1b4bd3b4c0ec18bafeff27b1cbacf7d889df724d
Humzah Abdol <humzah.abdol@alaskaair.com>
2020-09-03 23:21:09 -0700

    added video footer and style the video

---
+++ tiktok/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Video from "./Video";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__videos">
+        <Video />
+        <Video />
+      </div>
+
+      {/*  app container */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+    </div>
+  );
+}
+
+export default App;
