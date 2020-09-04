ed00c5898c84f2b22c8e15cbc523931d24d79e33
Humzah Abdol <humzah.abdol@alaskaair.com>
2020-09-04 03:23:49 -0700

    sidebar for tictok clone

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -3,7 +3,7 @@ import "./Video.css";
 import VideoFooter from "./VideoFooter";
 import VideoSidebar from "./VideoSidebar";
 
-function Video() {
+function Video({ url, likes, share, messages, channel, description, song }) {
   const videoRef = useRef(null);
   const [playing, setPlaying] = useState(false);
 
@@ -23,11 +23,10 @@ function Video() {
         className="video__player"
         loop
         ref={videoRef}
-        src="https://v16m.tiktokcdn.com/b0524d66018042fe438af61989ee30d5/5f51e18d/video/tos/useast2a/tos-useast2a-ve-0068c004/2362532506c24a66be8a02a69273732d/?a=1233&br=2652&bt=1326&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200904004105010189073077093221E8&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2tmOHM1Ojc6dTMzMzczM0ApZDpnZmVmM2QzNzM3OmVmaWdeYm1nLjE0bWlfLS1iMTZzczIuNTZeYDNgNTM2XmFhMDU6Yw%3D%3D&vl=&vr="
+        src={url}
       ></video>
-      <VideoFooter></VideoFooter>
-      {/* Video footer */}
-      {/* VideoSidebar  */}
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSidebar likes={likes} share={share} messages={messages} />
     </div>
   );
 }
