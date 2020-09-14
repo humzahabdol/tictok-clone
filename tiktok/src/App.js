ccf8da6e8b8e45acf98e42dcb01f81a5cca8d27d
Humzah Abdol <humzah.abdol@alaskaair.com>
2020-09-14 12:12:02 -0700

    cdn update

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="app">
       <div className="app__videos">
         <Video
-          url="https://v16m.tiktokcdn.com/26a83c6e99ccc68169ccd93c9048d3a1/5f524154/video/tos/useast2a/tos-useast2a-ve-0068c004/2362532506c24a66be8a02a69273732d/?a=1233&br=2652&bt=1326&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090407294401019018615451004497&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2tmOHM1Ojc6dTMzMzczM0ApZDpnZmVmM2QzNzM3OmVmaWdeYm1nLjE0bWlfLS1iMTZzczIuNTZeYDNgNTM2XmFhMDU6Yw%3D%3D&vl=&vr="
+          url="https://v77.tiktokcdn.com/87c28b1284c28bb0b79104104773d2fd/5f60137b/video/tos/useast2a/tos-useast2a-ve-0068c004/2362532506c24a66be8a02a69273732d/?a=1233&br=2652&bt=1326&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020091419055101018906515824022654&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2tmOHM1Ojc6dTMzMzczM0ApZDpnZmVmM2QzNzM3OmVmaWdeYm1nLjE0bWlfLS1iMTZzczIuNTZeYDNgNTM2XmFhMDU6Yw%3D%3D&vl=&vr="
           channel="huabdol"
           description="Hello World"
           song="99 problems and react aint one"
@@ -16,7 +16,7 @@ function App() {
           shares={789}
         />
         <Video
-          url="https://v16m.tiktokcdn.com/26a83c6e99ccc68169ccd93c9048d3a1/5f524154/video/tos/useast2a/tos-useast2a-ve-0068c004/2362532506c24a66be8a02a69273732d/?a=1233&br=2652&bt=1326&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090407294401019018615451004497&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2tmOHM1Ojc6dTMzMzczM0ApZDpnZmVmM2QzNzM3OmVmaWdeYm1nLjE0bWlfLS1iMTZzczIuNTZeYDNgNTM2XmFhMDU6Yw%3D%3D&vl=&vr="
+          url="https://v77.tiktokcdn.com/87c28b1284c28bb0b79104104773d2fd/5f60137b/video/tos/useast2a/tos-useast2a-ve-0068c004/2362532506c24a66be8a02a69273732d/?a=1233&br=2652&bt=1326&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020091419055101018906515824022654&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2tmOHM1Ojc6dTMzMzczM0ApZDpnZmVmM2QzNzM3OmVmaWdeYm1nLjE0bWlfLS1iMTZzczIuNTZeYDNgNTM2XmFhMDU6Yw%3D%3D&vl=&vr="
           channel="huabdol"
           description="Hello World"
           song="99 problems and react aint one"
